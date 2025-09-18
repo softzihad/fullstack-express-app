6384f90227247c1cf47c8209f103fc1c5c70918d
Zahidul Islam Zihad <soft.zihad@gmail.com>
2025-09-18 17:16:42 +0600

    setup frontend 2nd template use static middleware

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ import express from "express";
 
 const app = express();
 
-app.use(express.static('public/1st-template'));
+//app.use(express.static('public/1st-template'));
+app.use(express.static('public/2nd-template'));
 
 app.get("/", (req, res) => {
     res.send("Hello, Express!");
