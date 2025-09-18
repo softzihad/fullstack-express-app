12d1804245e53630ddaf5662d94984a0f9333d69
Zahidul Islam Zihad <soft.zihad@gmail.com>
2025-09-18 21:07:08 +0600

    Update logTable.js

--- logTable.js
+++ logTable.js
@@ -10,6 +10,12 @@ async function viewAllProducts() {
 
   try {
     const products = await db.all('SELECT * FROM products')
+
+    // Neater table display
+    const displayItems = products.map(({ id, title, artist, year, stock }) => {
+      return { id, title, artist, year, stock }
+    })
+
     console.table(products) 
   } catch (err) {
     console.error('Error fetching products:', err.message)
